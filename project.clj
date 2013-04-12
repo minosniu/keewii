91b8836ec4a46ae4f19a814878ceb7b2ef727eac
C. Minos Niu <minos.niu@gmail.com>
2013-04-12 15:17:21 -0700

    + project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject keewii "0.1.0-SNAPSHOT"
+  :description "A real-time speech synthesizier"
+  :url "http://keewii.github.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.7.1"]
+                 ;[incanter "1.5.0-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0"]])

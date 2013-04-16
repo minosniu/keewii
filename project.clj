e71d3677a4169dee5c3c60e1ccd00bca433273d5
KangWooLee <kangwool@usc.edu>
2013-04-16 03:25:16 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [overtone "0.7.1"]
-                 ;[incanter "1.5.0-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [overtone "0.7.1"]]
+  :compile-path "target/classes"
+  :source-paths      ["src/keewii"]
+  :java-source-paths ["src/java"]
+  :aot [keewii.core]
+  :main keewii.core)

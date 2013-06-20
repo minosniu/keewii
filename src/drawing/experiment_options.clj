1ff442435c48ddbdf2bc186f5f3e197932e68376
KangWooLee <kangwool@usc.edu>
2013-06-20 17:06:26 -0600

    Update experiment_options.clj

--- src/drawing/experiment_options.clj
+++ src/drawing/experiment_options.clj
@@ -1,61 +1,53 @@
 (ns drawing.experiment_options) 
 
 
-(import '(javax.swing JFrame JLabel JTextField JButton)
-        '(java.awt.event ActionListener)
-        '(java.awt GridLayout))
+(import '(javax.swing JFrame JLabel JTextField JButton JComboBox)
+        '(java.awt.event ActionListener ItemListener)
+        '(java.awt GridLayout)
+        '(java.util.Vector))
 
 (def CUR (atom true))
 (def TGT (atom true))
+(def options (atom true)) 
+(def start-trial (atom 1))
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+ 
 (defn OPTIONS []
   (let [frame (JFrame. "Experimental options")
         ;temp-text (JTextField.)
         ;celsius-label (JLabel. "Celsius")
-        option1 (JButton. "Cursor(O) + Target(O)")
-        option2 (JButton. "Cursor(O) + Target(X)")
-        option3 (JButton. "Cursor(X) + Target(O)")
-        option4 (JButton. "Cursor(X) + Target(X)")
-        ;convert-button (JButton. "Convert")
-        ;fahrenheit-label (JLabel. "Fahrenheit")
-        ]
+        option (JComboBox.  (java.util.Vector. ["Cursor+Target" "Cursor only" "Target only" "Nothing"])) 
+        Name (JTextField. "Type your name here.")
+        Start (JTextField. "Set the trial number to start from. (1~25)") 
+        Submit (JButton. "Submit") ]
+    (.addItemListener
+      option
+       (proxy [java.awt.event.ItemListener] []
+                      (itemStateChanged [item-event]
+                        (case (.getSelectedIndex option)
+                          0 (doseq [] (reset! CUR true) (reset! TGT true)) 
+                          1 (doseq [] (reset! CUR true) (reset! TGT false))
+                          2 (doseq [] (reset! CUR false) (reset! TGT true))
+                          3 (doseq [] (reset! CUR false) (reset! TGT false)))))) 
     (.addActionListener
-     option1
-     (reify ActionListener 
-       (actionPerformed
-         [_ evt]
-         (reset! CUR true)
-         (reset! TGT true)
-       
-         )))
-    (.addActionListener
-      option2
+      Submit
       (reify ActionListener 
         (actionPerformed
           [_ evt]
-          (reset! CUR true)
-          (reset! TGT false)
-          )))
-    (.addActionListener
-      option3
-      (reify ActionListener 
-        (actionPerformed
-          [_ evt]
-          (reset! CUR false)
-          (reset! TGT true)
-          )))
-    (.addActionListener
-      option4
-      (reify ActionListener 
-        (actionPerformed
-          [_ evt]
-             (reset! CUR false)
-             (reset! TGT false)
-             )))
+          (def NAME (.getText Name))
+           (reset! start-trial (parse-int (.getText Start)) ) 
+          (.dispose frame) 
+          (reset! options false))))
+
     (doto frame
       (.setLayout (GridLayout. 4 1 2 2))
-      (.add option1)
-      (.add option2)
-      (.add option3)
-      (.add option4)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) 
+      (.setLocation 500 300) 
+      (.add option)
+      (.add Name)
+      (.add Start)
+      (.add Submit)
       (.setSize 300 300)
       (.setVisible true))))
+;(OPTIONS)

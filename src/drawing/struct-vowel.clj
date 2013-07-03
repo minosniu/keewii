d98c313bff91ee16451fe01a730c138b5b158a89
KangWooLee <kangwool@usc.edu>
2013-07-03 15:55:14 -0700

    Update struct-vowel.clj

--- src/drawing/struct-vowel.clj
+++ src/drawing/struct-vowel.clj
@@ -1,12 +1,22 @@
-(ns drawing.struct-vowel)
-
+(ns drawing.struct_vowel
+  (:require [clojure.string :as string]))
 ;basic vowels
 (defstruct vowel :name :f1 :f2)
-(def A (struct vowel "a" 750 1000))
-(def E (struct vowel "e" 530 1840))
-(def I (struct vowel "i" 280 2250))
-(def O (struct vowel "o" 370 730))
-(def U (struct vowel "u" 290 680))
-(def S (struct vowel "Test will start soon" 500 1800))
+(def A (struct vowel "a" 750 1000));750/1000
+(def E (struct vowel "e" 530 1840));530/1840
+(def I (struct vowel "i" 290 2350));280/2250
+(def O (struct vowel "o" 440 650)) ;370/730
+(def U (struct vowel "u" 220 500));290/680
+(def S (struct vowel "Test will start soon" 500 1700))
 ;(:ind vowel)
 (def alphabet (atom S))
+
+;(shuffle  (reduce into (map #(repeat 5 %) ["u" "o" "i" "a" "e"]))  )
+(defn matcher [CH]
+  "This function converts char to vowel structure"
+  (case CH
+    "a" A
+    "e" E
+    "i" I
+    "o" O
+    "u" U))

d63c0bf9dd47048c0243bc3a0281c9345a4c3fa1
KangWooLee <kangwool@usc.edu>
2013-07-10 15:26:37 -0700

    Update experiment_options.clj

--- src/drawing/experiment_options.clj
+++ src/drawing/experiment_options.clj
@@ -5,19 +5,58 @@
         '(java.awt.event ActionListener ItemListener)
         '(java.awt GridLayout)
         '(java.util.Vector))
-
-(def CUR (atom true))
-(def TGT (atom true))
-(def options (atom true)) 
 (def start-trial (atom 1))
 (defn parse-int [s]
    (Integer. (re-find  #"\d+" s )))
- 
+(def options (atom true)) 
+(def CUR (atom true))
+(def TGT (atom true))
+(def POLAR_COORDINATES (atom false))
+;For Cursor&Target options
+;(defn OPTIONS []
+;  (let [frame (JFrame. "Experimental options")
+;        ;temp-text (JTextField.)
+;        ;celsius-label (JLabel. "Celsius")
+;        option (JComboBox.  (java.util.Vector. ["Cursor+Target" "Cursor only" "Target only" "Nothing"])) 
+;        Name (JTextField. "Type your name here.")
+;        Start (JTextField. "Set the trial number to start from. (1~25)") 
+;        Submit (JButton. "Submit") ]
+;    (.addItemListener
+;      option
+;       (proxy [java.awt.event.ItemListener] []
+;                      (itemStateChanged [item-event]
+;                        (case (.getSelectedIndex option)
+;                          0 (doseq [] (reset! CUR true) (reset! TGT true)) 
+;                          1 (doseq [] (reset! CUR true) (reset! TGT false))
+;                          2 (doseq [] (reset! CUR false) (reset! TGT true))
+;                          3 (doseq [] (reset! CUR false) (reset! TGT false)))))) 
+;    (.addActionListener
+;      Submit
+;      (reify ActionListener 
+;        (actionPerformed
+;          [_ evt]
+;          (def NAME (.getText Name))
+;           (reset! start-trial (parse-int (.getText Start)) ) 
+;          (.dispose frame) 
+;          (reset! options false))))
+;
+;    (doto frame
+;      (.setLayout (GridLayout. 4 1 2 2))
+;      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) 
+;      (.setLocation 500 300) 
+;      (.add option)
+;      (.add Name)
+;      (.add Start)
+;      (.add Submit)
+;      (.setSize 300 300)
+;      (.setVisible true))))
+
+;coordinate options
 (defn OPTIONS []
   (let [frame (JFrame. "Experimental options")
         ;temp-text (JTextField.)
         ;celsius-label (JLabel. "Celsius")
-        option (JComboBox.  (java.util.Vector. ["Cursor+Target" "Cursor only" "Target only" "Nothing"])) 
+        option (JComboBox.  (java.util.Vector. ["Cartesian coordinate" "Polar coordinate"])) 
         Name (JTextField. "Type your name here.")
         Start (JTextField. "Set the trial number to start from. (1~25)") 
         Submit (JButton. "Submit") ]
@@ -26,10 +65,8 @@
        (proxy [java.awt.event.ItemListener] []
                       (itemStateChanged [item-event]
                         (case (.getSelectedIndex option)
-                          0 (doseq [] (reset! CUR true) (reset! TGT true)) 
-                          1 (doseq [] (reset! CUR true) (reset! TGT false))
-                          2 (doseq [] (reset! CUR false) (reset! TGT true))
-                          3 (doseq [] (reset! CUR false) (reset! TGT false)))))) 
+                          0 (doseq [] (reset! POLAR_COORDINATES false))
+                          1 (doseq [] (reset! POLAR_COORDINATES true)))))) 
     (.addActionListener
       Submit
       (reify ActionListener 
@@ -50,4 +87,5 @@
       (.add Submit)
       (.setSize 300 300)
       (.setVisible true))))
+
 ;(OPTIONS)

a0538bce665627e01993da71a2084049e04a04a4
KangWooLee <kangwool@usc.edu>
2013-07-16 17:13:27 -0700

    Create initializing.clj

---
+++ src/drawing/initializing.clj
@@ -0,0 +1,40 @@
+(ns drawing.initializing
+  (:require [clojure.string :as string])
+  (:use [overtone.at-at :only (now)]
+        [drawing.experiment_options]
+        [drawing.toolbox]
+        [drawing.struct_vowel]
+        [drawing.basic_vars] ) 
+  (:import (java.io File)))
+
+;Experimental options & subject info
+(OPTIONS)
+(while @options)
+
+;(def Condition (atom (str (if @CUR "C" "") (if @TGT "T" "") (if (not (or @CUR @TGT)) "none")))) ;Cur+Tgt options
+(def Condition (atom (if @POLAR_COORDINATES "Polar" "Cart"))) 
+;(defonce DATE (get-date))
+;(def FILENAME (str "C:\\Code\\keewii1\\data\\" NAME DATE "\\" @Condition "\\")) ;In case we need date
+
+;File & Directory info
+(def FILENAME (str "C:\\Code\\keewii1\\data\\" NAME "\\" @Condition "\\"))
+(def Temp_data "C:\\Code\\keewii1\\temp\\") 
+(def session_number (atom 0))
+
+;Make directories
+(mkdir FILENAME)
+(mkdir (str FILENAME "\\dat\\"))
+(mkdir (str FILENAME "\\wav\\"))
+
+(defn seq_check [filename]
+  "This function checks if seq.txt exists" 
+  (let [f (File. filename)]
+    (cond
+      (.isFile f)      (def randomized-sequence 
+                         (into [] (map #(matcher %) (string/split (slurp (str FILENAME "seq.txt")) #"\s+"))) );"file"
+      (.isDirectory f) "directory"
+      (.exists f)      "other"
+      :else            (doseq [] (def randomized-sequence  ;non existent case
+                                   (shuffle (reduce into (map #(repeat 5 %) [A E I O U])))) 
+                         (spit (str FILENAME "seq.txt") 
+                               (string/join " " (reduce str (map #(% :name) randomized-sequence))))))))

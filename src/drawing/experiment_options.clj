8c524b35aba9b23d9d8d9c2b72a1794e9b6dab45
KangWooLee <kangwool@usc.edu>
2013-07-17 17:33:18 -0700

    Update experiment_options.clj

--- src/drawing/experiment_options.clj
+++ src/drawing/experiment_options.clj
@@ -2,51 +2,9 @@
   (:use [drawing.toolbox :only (parse-int)]
         [drawing.basic_vars])
   (:import (javax.swing JFrame JLabel JTextField JButton JComboBox)
-           (java.awt.event ActionListener ItemListener)
+           (java.awt.event ActionListener ItemListener MouseListener MouseAdapter KeyListener)
            (java.awt GridLayout)
            (java.util.Vector))) 
-
-;(def CUR (atom true))
-;(def TGT (atom true))
-
-;For Cursor&Target options
-;(defn OPTIONS []
-;  (let [frame (JFrame. "Experimental options")
-;        ;temp-text (JTextField.)
-;        ;celsius-label (JLabel. "Celsius")
-;        option (JComboBox.  (java.util.Vector. ["Cursor+Target" "Cursor only" "Target only" "Nothing"])) 
-;        Name (JTextField. "Type your name here.")
-;        Start (JTextField. "Set the trial number to start from. (1~25)") 
-;        Submit (JButton. "Submit") ]
-;    (.addItemListener
-;      option
-;       (proxy [java.awt.event.ItemListener] []
-;                      (itemStateChanged [item-event]
-;                        (case (.getSelectedIndex option)
-;                          0 (doseq [] (reset! CUR true) (reset! TGT true)) 
-;                          1 (doseq [] (reset! CUR true) (reset! TGT false))
-;                          2 (doseq [] (reset! CUR false) (reset! TGT true))
-;                          3 (doseq [] (reset! CUR false) (reset! TGT false)))))) 
-;    (.addActionListener
-;      Submit
-;      (reify ActionListener 
-;        (actionPerformed
-;          [_ evt]
-;          (def NAME (.getText Name))
-;           (reset! start-trial (parse-int (.getText Start)) ) 
-;          (.dispose frame) 
-;          (reset! options false))))
-;
-;    (doto frame
-;      (.setLayout (GridLayout. 4 1 2 2))
-;      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) 
-;      (.setLocation 500 300) 
-;      (.add option)
-;      (.add Name)
-;      (.add Start)
-;      (.add Submit)
-;      (.setSize 300 300)
-;      (.setVisible true))))
 
 ;coordinate options
 (defn OPTIONS []
@@ -71,7 +29,20 @@
            (reset! start-trial (parse-int (.getText Start)) ) 
           (.dispose frame) 
           (reset! options false))))
+    (.addMouseListener 
+      Name
+      (proxy [MouseAdapter] []
+        (mouseClicked [e]
+          (.setText Name "")
+          (.setToolTipText Name "Name")))) 
+    (.addMouseListener 
+      Start
+      (proxy [MouseAdapter] []
+        (mouseClicked [e]
+          (.setText Start "1")
+          (.setToolTipText Start "Starting point")))) 
 
+    
     (doto frame
       (.setLayout (GridLayout. 4 1 2 2))
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) 
@@ -80,7 +51,7 @@
       (.add Name)
       (.add Start)
       (.add Submit)
-      (.setSize 300 300)
+      (.setSize 400 300)
       (.setVisible true))))
 
 (defn Free_Ex_Op []
@@ -107,7 +78,7 @@
       (.setLocation 500 300) 
       (.add option)
       (.add Submit)
-      (.setSize 300 100)
+      (.setSize 400 120)
       (.setVisible true))))
 
 ;(OPTIONS)
